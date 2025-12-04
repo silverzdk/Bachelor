fixed enthalpy calculation

src/Simple_Model_r0.py
@@ -33,15 +33,16 @@ class Fluid(pf.Fluid):
         new_fluid.set_geometry(self.Geometry, self.flow_type)
         new_fluid.update(Input1, Input2)
         return new_fluid
-    
+
     def liquid_phase(self):
         '''Returns a new Fluid object in the liquid phase at the current pressure'''
         if self.phase.name != "TwoPhase":
             raise ValueError("Fluid is not in a two-phase state.")
-        
+
         liquid_phase = Fluid(self.name, self.m_dot)
         liquid_phase.set_geometry(self.Geometry, self.flow_type)
         liquid_phase.update(Input.quality(0), Input.pressure(self.pressure))
+       
         return liquid_phase
     
     # Returns a new Fluid object in the vapor phase at the current pressure
@@ -51,8 +52,9 @@ class Fluid(pf.Fluid):
             raise ValueError("Fluid is not in a two-phase state.")
         
         vapor_phase = Fluid(self.name, self.m_dot)
+        vapor_phase.set_geometry(self.Geometry,self.flow_type)        
         vapor_phase.update(Input.quality(1), Input.pressure(self.pressure))
-        vapor_phase.set_geometry(self.Geometry,self.flow_type)
+        
         return vapor_phase
     
     def __repr__(self):
@@ -76,7 +78,6 @@ class Fluid(pf.Fluid):
             
         elif self.flow_type == 'external':
             u = self.m_dot / (self.density * self.Geometry.duct_area)
-        
             Re = u  * self.Geometry.outer_diameter / self.kinematic_viscosity
             
         else :
@@ -101,36 +102,37 @@ class Fluid(pf.Fluid):
 class Geometry:
     '''Class representing the geometry of the heat exchanger with given dimensions'''
     def __init__(self,width,length,pipe_outer_diameter,pipe_wall_thickness,transverse_pitch,longitudinal_pitch,arrangement="Inline",angle=0):
-        self.width = width
-        self.length = length
-        self.outer_diameter = pipe_outer_diameter
-        self.wall_thickness = pipe_wall_thickness
-        self.transverse_pitch = transverse_pitch
+        self.width              = width
+        self.length             = length
+        self.outer_diameter     = pipe_outer_diameter
+        self.wall_thickness     = pipe_wall_thickness
+        self.transverse_pitch   = transverse_pitch
         self.longitudinal_pitch = longitudinal_pitch
-        self.arrangement = arrangement
-        self.angle          = angle
+        self.arrangement        = arrangement
+        self.angle              = angle
         
         # Calculate derived properties
         # tube properties
-        self.inner_diameter  = self.outer_diameter - 2 * self.wall_thickness
-        self.inner_perimeter = np.pi * self.inner_diameter
-        self.outer_perimeter = np.pi * self.outer_diameter
+        self.inner_diameter     = self.outer_diameter - 2 * self.wall_thickness
+        self.inner_perimeter    = np.pi * self.inner_diameter
+        self.outer_perimeter    = np.pi * self.outer_diameter
         
         # tube bank properties
-        if arrangement == "Inline":
+        if arrangement          == "Inline":
             self.diagonal_pitch = None
-        elif arrangement == "Staggered":
+        elif arrangement        == "Staggered":
             self.diagonal_pitch = np.sqrt(self.longitudinal_pitch**2+(self.transverse_pitch/2)**2)
         
         # Duct properties
-        self.duct_area       = self.width * self.length
+        self.duct_area           = self.width * self.length
 
 class Node:
-    def __init__(self,Geometry,x_pos,node_length,T_hot_init,T_cold_init,P_hot_init,P_cold_init,m_dot_hot,m_dot_cold,is_boundary=False):
-        self.Geometry = Geometry
-        self.is_boundary = is_boundary
-        self.x_pos = x_pos
-        self.node_length = node_length
+    def __init__(self,Geometry,x_pos,node_length,T_hot_init,T_cold_init,P_hot_init,P_cold_init,m_dot_hot,m_dot_cold,roughness,is_boundary=False):
+        self.Geometry       = Geometry
+        self.is_boundary    = is_boundary
+        self.x_pos          = x_pos
+        self.node_length    = node_length
+        self.roughness      = roughness
 
         # Hot fluid
         self.Fluid_hot = Fluid(FluidsList.Water, m_dot_hot)
@@ -147,19 +149,22 @@ class Node:
         # If not two-phase
         if self.Fluid_hot.phase.name != "TwoPhase":
             NotImplementedError("Single phase heat transfer not implemented yet.")
-            self.h_h = -np.ln((-Tmx + T_s)/(-Tm_in + T_s))*m_dot*c_P/(P*x)
-        else:
-           # Shah correlation for boiling inside tubes
-           liquid_phase = self.Fluid_hot.liquid_phase()
+            
+        #     self.h_h = -np.ln((-Tmx + T_s)/(-T + T_s))*m_dot*c_P/(P*x)
 
-           # Shah 1979 eq 5
-           h_L = (0.023*liquid_phase.Re**(0.8)*liquid_phase.Pr**(0.4)*liquid_phase.k)/self.Geometry.inner_diameter
+        # Shah correlation for boiling inside tubes
+        liquid_phase = self.Fluid_hot.liquid_phase()
+
+        # Shah 1979 eq 5
+        h_L = (0.023*liquid_phase.Re**(0.8)*liquid_phase.Pr**(0.4)*liquid_phase.k)/self.Geometry.inner_diameter
            
-           x = self.Fluid_hot.quality
-           
-           # Shah 1979 eq 8
-           h_TP = h_L * ((1-x)**0.8 + (3.8*x**0.76 * (1-x)**0.04)/(self.Fluid_hot.Pr**0.38))
-           self.h_h = h_TP
+        x = self.Fluid_hot.quality
+        p_crit   = self.Fluid_hot.critical_pressure
+        p_r      = self.Fluid_hot.pressure / p_crit
+
+        # Shah 1979 eq 8
+        h_TP = h_L * ((1-x)**0.8 + (3.8*x**0.76 * (1-x)**0.04)/(p_r**0.38))
+        self.h_h = h_TP
            
     def __outer_convective_HTC__(self):        
         Nu_D = 0.3 + (
@@ -169,21 +174,28 @@ class Node:
     def overall_HTC(self):
         self.__inner_convective_HTC__()
         self.__outer_convective_HTC__()
-        
+    
         # Inner overall heat transfer coefficient 
         # From heat transfer in single and multiphase systems (10.69) p. 495
-        U_i = np.abs(np.pow((1/self.h_c + self.Geometry.inner_diameter/(self.Geometry.outer_diameter*self.h_h)),-1))
-            
+        U_i = np.power((1/self.h_c + self.Geometry.inner_diameter/(self.Geometry.outer_diameter*self.h_h)),-1)
+        
         inner_area = self.Geometry.inner_perimeter * self.node_length
-        Delta_H =( U_i * inner_area )/self.Fluid_cold.m_dot * (self.Fluid_hot.temperature - self.Fluid_cold.temperature)
-        
-        return Delta_H
-        
+        Delta_H = (U_i * inner_area) / self.Fluid_hot.m_dot * (self.Fluid_cold.temperature - self.Fluid_hot.temperature)
+    
+        return [Delta_H, U_i]
         
     def pressure_drop(self):
-        pass
-    
-    
+        
+        # Churchill equation for finding the friction factor
+        A   = (-2.457*np.ln((7/self.flow_type.Re)**0.9+((0.27*eps)/d)))**(16)
+        B   = (37530/self.flow_type.Re)**(16)
+        f   = 8 * ((8/self.flow_type.Re)**(12)+(1/((A+B)**(3/2))))**(1/12)
+
+        # Headloss
+        DeltaP_L    = (f*L)/(self.inner_diameter) * (self.fluid_hot.density)/(1)
+
+        pass     
+
 if __name__ == "__main__":
     geom = Geometry(
     # Height and length of the heat exchanger
@@ -206,13 +218,13 @@ if __name__ == "__main__":
         T_cold_init=200, # C 
         P_hot_init=158e5, # Pa
         P_cold_init=1e5, # Pa
-        m_dot_hot=6, # kg/s
-        m_dot_cold=50, # kg/s
+        m_dot_hot=0.1, # kg/s
+        m_dot_cold=11.04, # kg/s
         is_boundary=True
     )
 
 
-    node1.Fluid_hot.update(Input.pressure(158e5), Input.enthalpy(node1.Fluid_hot.dew_point_at_pressure(158e5).enthalpy-1000))
+    node1.Fluid_hot.update(Input.pressure(158e5), Input.quality(0.98))
     
     Delta_H = node1.overall_HTC()
     print(Delta_H)